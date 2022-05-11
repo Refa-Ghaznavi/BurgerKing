6ea72afbfc24c32bcdc8e7b1b7072cfb915931d7
refa-ghaznavi <refaghaznavi@gmail.com>
2022-05-11 16:41:45 +0430

    adding fragments for specific orders

---
+++ app/src/main/java/dev/refa/burgerking/PickupFragment.kt
@@ -0,0 +1,71 @@
+package dev.refa.burgerking
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.activityViewModels
+import androidx.navigation.fragment.findNavController
+import dev.refa.burgerking.databinding.FragmentPickupBinding
+import dev.refa.burgerking.model.OrderViewModel
+
+/**
+ * [PickupFragment] allows the user to choose a pickup date for the cupcake order.
+ */
+class PickupFragment : Fragment() {
+
+    // Binding object instance corresponding to the fragment_pickup.xml layout
+    // This property is non-null between the onCreateView() and onDestroyView() lifecycle callbacks,
+    // when the view hierarchy is attached to the fragment.
+    private var binding: FragmentPickupBinding? = null
+
+    // Use the 'by activityViewModels()' Kotlin property delegate from the fragment-ktx artifact
+    private val sharedViewModel: OrderViewModel by activityViewModels()
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        val fragmentBinding = FragmentPickupBinding.inflate(inflater, container, false)
+        binding = fragmentBinding
+        return fragmentBinding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding?.apply {
+            lifecycleOwner = viewLifecycleOwner
+            viewModel = sharedViewModel
+            pickupFragment = this@PickupFragment
+        }
+    }
+
+    /**
+     * Navigate to the next screen to see the order summary.
+     */
+    fun goToNextScreen() {
+        findNavController().navigate(R.id.action_pickupFragment_to_summaryFragment)
+    }
+
+    /**
+     * Cancel the order and start over.
+     */
+    fun cancelOrder() {
+        // Reset order in view model
+        sharedViewModel.resetOrder()
+
+        // Navigate back to the [StartFragment] to start over
+        findNavController().navigate(R.id.action_pickupFragment_to_startFragment)
+    }
+
+    /**
+     * This fragment lifecycle method is called when the view hierarchy associated with the fragment
+     * is being removed. As a result, clear out the binding object.
+     */
+    override fun onDestroyView() {
+        super.onDestroyView()
+        binding = null
+    }
+}
---
+++ app/src/main/java/dev/refa/burgerking/StartFragment.kt
@@ -0,0 +1,64 @@
+package dev.refa.burgerking
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.activityViewModels
+import androidx.navigation.fragment.findNavController
+import dev.refa.burgerking.databinding.FragmentStartBinding
+import dev.refa.burgerking.model.OrderViewModel
+
+/**
+ * This is the first screen of the Cupcake app. The user can choose how many cupcakes to order.
+ */
+class StartFragment : Fragment() {
+
+    // Binding object instance corresponding to the fragment_start.xml layout
+    // This property is non-null between the onCreateView() and onDestroyView() lifecycle callbacks,
+    // when the view hierarchy is attached to the fragment.
+    private var binding: FragmentStartBinding? = null
+
+    // Use the 'by activityViewModels()' Kotlin property delegate from the fragment-ktx artifact
+    private val sharedViewModel: OrderViewModel by activityViewModels()
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        val fragmentBinding = FragmentStartBinding.inflate(inflater, container, false)
+        binding = fragmentBinding
+        return fragmentBinding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding?.startFragment = this
+    }
+
+    /**
+     * Start an order with the desired quantity of cupcakes and navigate to the next screen.
+     */
+    fun orderCupcake(quantity: Int) {
+        // Update the view model with the quantity
+        sharedViewModel.setQuantity(quantity)
+
+        // If no flavor is set in the view model yet, select vanilla as default flavor
+        if (sharedViewModel.hasNoFlavorSet()) {
+            sharedViewModel.setFlavor(getString(R.string.vanilla))
+        }
+
+        // Navigate to the next destination to select the flavor of the cupcakes
+        findNavController().navigate(R.id.action_startFragment_to_flavorFragment)
+    }
+
+    /**
+     * This fragment lifecycle method is called when the view hierarchy associated with the fragment
+     * is being removed. As a result, clear out the binding object.
+     */
+    override fun onDestroyView() {
+        super.onDestroyView()
+        binding = null
+    }
+}
